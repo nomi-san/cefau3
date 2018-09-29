f4a076c8a71f31358668bc57124ed194b99e2f27
Duy .Ng <38210249+wy3@users.noreply.github.com>
2018-09-29 23:53:59 +0700

    update

--- src/au3obj_wrap.h
+++ src/au3obj_wrap.h
@@ -1,1 +1,20 @@
+#ifndef AU3OBJ_WRAP_INCLUDE
+#define AU3OBJ_WRAP_INCLUDE
+#pragma once
 
+#include "cefau3.h"
+
+#ifdef _WIN32
+#define __Au3Obj_dll "AutoItObject.dll"
+#else
+#define __Au3Obj_dll "AutoItObject_X64.dll"
+#endif
+
+static void* __Au3Obj_module = NULL;
+
+static func_std(CefAu3Obj_AddMethod, void, void* object, wchar_t* method, wchar_t* value, int new_scope);
+static func_std(CefAu3Obj_AddProperty, void, void* object, wchar_t* property_name, int new_scope, void* property_value);
+static func_std(CefAu3Obj_CloneAutoItObject, void*, void* parent);
+static func_std(CefAu3Obj_CreateAutoItObject, void*);
+
+#endif
